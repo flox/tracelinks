46108503f52bc2fcc948abb9b00fc65a13e5f5bd
Michael Brantley <brantley@floxdev.com>
2023-01-27 19:20:49 +0000

    updated proto derivation to use latest flake.nix

--- pkgs/tracelinks/default.nix
+++ pkgs/tracelinks/default.nix
@@ -1,7 +1,7 @@
 { self, stdenv, help2man, inputs }:
 stdenv.mkDerivation {
   pname = "tracelinks";
-  version = "1.0.0-${inputs.floxpkgs.lib.getRev self}";
+  version = "1.0.0-${inputs.flox-floxpkgs.lib.getRev self}";
   src = self;
 
   # Prevent the source from becoming a runtime dependency.
