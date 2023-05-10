da75c136fb8ddea51abef9e70e664127b5d65fbe
Rok Garbas <rok@floxdev.com>
2023-05-10 14:57:48 +0200

    build: use flox/flox instead of flox/floxpkgs to setup a project

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
   description = "Floxpkgs/Project Template";
-  nixConfig.bash-prompt = "[flox] \\[\\033[38;5;172m\\]λ \\[\\033[0m\\]";
-  inputs.flox-floxpkgs.url = "github:flox/floxpkgs";
+
+  inputs.flox.url = "github:flox/flox";
 
   # Declaration of external resources
   # =================================
 
   # =================================
 
-  outputs = args @ {flox-floxpkgs, ...}: flox-floxpkgs.project args (_: {});
+  outputs = args @ {flox, ...}: flox.project args (_: {});
 }
