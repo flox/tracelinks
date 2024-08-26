391d4fe09163039f2992ff0151e42efc00885153
Tom Bereknyei <tomberek@gmail.com>
2024-08-26 02:16:15 -0400

    chore: remove extra inputs

--- flake.nix
+++ flake.nix
@@ -1,27 +1,12 @@
 {
   description = "List symbolic links encountered in path traversal.";
 
-  inputs = {
-    nixpkgs.url = "github:flox/nixpkgs";
-    utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "github:flox/nixpkgs";
 
-  outputs =
-    { nixpkgs, ... }@inputs:
-    inputs.utils.lib.eachSystem
-      [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ]
-      (
-        system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          packages.default = pkgs.callPackage ./pkgs/tracelinks/default.nix { };
-        }
-      );
+  outputs = inputs: {
+    recipes.tracelinks = import ./pkgs/tracelinks;
+    packages = builtins.mapAttrs (system: pkgs: {
+      default = pkgs.callPackage inputs.self.recipes.tracelinks { };
+    }) inputs.nixpkgs.legacyPackages;
+  };
 }
